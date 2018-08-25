f27e47b6278445a5b67ba1692c0706bb9eaabb30
Eduardo Marçal <edumarcalnet@gmail.com>
2018-08-25 11:23:11 -0300

    Tests grouped by context.

--- operators_element_test.go
+++ operators_element_test.go
@@ -12,29 +12,31 @@ var _ = Describe("Operators Element", func() {
 		Expect(clearSession()).To(BeNil())
 	})
 
-	It("should find objects with an $exists condition", func() {
-		r := &testRepNoDefaultCriteriaNoDefaultSorting{}
-		insertObjects(r)
-		objs := make([]testRepObject, 0)
-		Expect(repository.FindAll(r, &objs, repository.And(
-			repository.WithCriteria(
-				repository.Exists("status", true),
-			),
-		))).To(BeNil())
-		Expect(objs).To(HaveLen(1))
-		Expect(objs[0].Name).To(Equal("Snake Eyes"))
-	})
+	Context("Operator: $exists", func() {
+		It("should find objects with an $exists condition", func() {
+			r := &testRepNoDefaultCriteriaNoDefaultSorting{}
+			insertObjects(r)
+			objs := make([]testRepObject, 0)
+			Expect(repository.FindAll(r, &objs, repository.And(
+				repository.WithCriteria(
+					repository.Exists("status", true),
+				),
+			))).To(BeNil())
+			Expect(objs).To(HaveLen(1))
+			Expect(objs[0].Name).To(Equal("Snake Eyes"))
+		})
 
-	It("should find not exists objects with an $exists", func() {
-		r := &testRepNoDefaultCriteriaNoDefaultSorting{}
-		insertObjects(r)
-		objs := make([]testRepObject, 0)
-		Expect(repository.FindAll(r, &objs, repository.Not(
-			repository.Exists("status", true),
-		))).To(BeNil())
-		Expect(objs).To(HaveLen(2))
-		Expect(objs[0].Name).To(Equal("Scarlett"))
-		Expect(objs[1].Name).To(Equal("Duke"))
+		It("should find not exists objects with an $exists", func() {
+			r := &testRepNoDefaultCriteriaNoDefaultSorting{}
+			insertObjects(r)
+			objs := make([]testRepObject, 0)
+			Expect(repository.FindAll(r, &objs, repository.Not(
+				repository.Exists("status", true),
+			))).To(BeNil())
+			Expect(objs).To(HaveLen(2))
+			Expect(objs[0].Name).To(Equal("Scarlett"))
+			Expect(objs[1].Name).To(Equal("Duke"))
+		})
 	})
 
 })
--- operators_logical_test.go
+++ operators_logical_test.go
@@ -12,21 +12,7 @@ var _ = Describe("Operators Logical", func() {
 		Expect(clearSession()).To(BeNil())
 	})
 
-	It("should find objects with an $and condition", func() {
-		r := &testRepNoDefaultCriteriaNoDefaultSorting{}
-		insertObjects(r)
-		objs := make([]testRepObject, 0)
-		Expect(repository.FindAll(r, &objs, repository.And(
-			repository.WithCriteria(
-				repository.LT("strength", 7),
-				repository.GT("agility", 8),
-			),
-		))).To(BeNil())
-		Expect(objs).To(HaveLen(1))
-		Expect(objs[0].Name).To(Equal("Scarlett"))
-	})
-
-	It("should find objects with an $and and a criteria with a erroneous condition", func() {
+	It("should find objects with a criteria with a erroneous condition", func() {
 		r := &testRepNoDefaultCriteriaNoDefaultSorting{}
 		insertObjects(r)
 		objs := make([]testRepObject, 0)
@@ -39,84 +25,106 @@ var _ = Describe("Operators Logical", func() {
 		Expect(err.Error()).To(ContainSubstring("forced error"))
 	})
 
-	It("should find objects with an $nor condition", func() {
-		r := &testRepNoDefaultCriteriaNoDefaultSorting{}
-		insertObjects(r)
-		objs := make([]testRepObject, 0)
-		Expect(repository.FindAll(r, &objs, repository.Nor(
-			repository.WithCriteria(
-				repository.EQ("strength", 5),
-			),
-		))).To(BeNil())
-		Expect(objs).To(HaveLen(2))
-		Expect(objs[0].Name).To(Equal("Snake Eyes"))
+	Context("Operator: $and", func() {
+		It("should find objects with an $and condition", func() {
+			r := &testRepNoDefaultCriteriaNoDefaultSorting{}
+			insertObjects(r)
+			objs := make([]testRepObject, 0)
+			Expect(repository.FindAll(r, &objs, repository.And(
+				repository.WithCriteria(
+					repository.LT("strength", 7),
+					repository.GT("agility", 8),
+				),
+			))).To(BeNil())
+			Expect(objs).To(HaveLen(1))
+			Expect(objs[0].Name).To(Equal("Scarlett"))
+		})
 	})
 
-	It("should find objects with an $nor and a criteria with a erroneous condition", func() {
-		r := &testRepNoDefaultCriteriaNoDefaultSorting{}
-		insertObjects(r)
-		objs := make([]testRepObject, 0)
-		err := repository.FindAll(r, &objs, repository.Nor(
-			repository.WithCriteria(
-				&erroneousCondition{},
-			),
-		))
-		Expect(err).NotTo(BeNil())
-		Expect(err.Error()).To(ContainSubstring("forced error"))
+	Context("Operator: $nor", func() {
+		It("should find objects with an $nor condition", func() {
+			r := &testRepNoDefaultCriteriaNoDefaultSorting{}
+			insertObjects(r)
+			objs := make([]testRepObject, 0)
+			Expect(repository.FindAll(r, &objs, repository.Nor(
+				repository.WithCriteria(
+					repository.EQ("strength", 5),
+				),
+			))).To(BeNil())
+			Expect(objs).To(HaveLen(2))
+			Expect(objs[0].Name).To(Equal("Snake Eyes"))
+		})
+
+		It("should find objects with an $nor and a criteria with a erroneous condition", func() {
+			r := &testRepNoDefaultCriteriaNoDefaultSorting{}
+			insertObjects(r)
+			objs := make([]testRepObject, 0)
+			err := repository.FindAll(r, &objs, repository.Nor(
+				repository.WithCriteria(
+					&erroneousCondition{},
+				),
+			))
+			Expect(err).NotTo(BeNil())
+			Expect(err.Error()).To(ContainSubstring("forced error"))
+		})
 	})
 
-	It("should find objects with an $or condition", func() {
-		r := &testRepNoDefaultCriteriaNoDefaultSorting{}
-		insertObjects(r)
-		objs := make([]testRepObject, 0)
-		Expect(repository.FindAll(r, &objs, repository.Or(
-			repository.WithCriteria(
-				repository.NE("strength", 5),
-				repository.EQ("age", 33),
-			),
-		))).To(BeNil())
-		Expect(objs).To(HaveLen(1))
-		Expect(objs[0].Name).To(Equal("Snake Eyes"))
-		Expect(objs[0].Age).To(Equal(33))
-		Expect(objs[0].Strength).To(Equal(7))
-		Expect(objs[0].Agiliy).To(Equal(9))
+	Context("Operator: $or", func() {
+		It("should find objects with an $or condition", func() {
+			r := &testRepNoDefaultCriteriaNoDefaultSorting{}
+			insertObjects(r)
+			objs := make([]testRepObject, 0)
+			Expect(repository.FindAll(r, &objs, repository.Or(
+				repository.WithCriteria(
+					repository.NE("strength", 5),
+					repository.EQ("age", 33),
+				),
+			))).To(BeNil())
+			Expect(objs).To(HaveLen(1))
+			Expect(objs[0].Name).To(Equal("Snake Eyes"))
+			Expect(objs[0].Age).To(Equal(33))
+			Expect(objs[0].Strength).To(Equal(7))
+			Expect(objs[0].Agiliy).To(Equal(9))
+		})
+
+		It("should find objects with an $or and a criteria with a erroneous condition", func() {
+			r := &testRepNoDefaultCriteriaNoDefaultSorting{}
+			insertObjects(r)
+			objs := make([]testRepObject, 0)
+			err := repository.FindAll(r, &objs, repository.Or(
+				repository.WithCriteria(
+					&erroneousCondition{},
+				),
+			))
+			Expect(err).NotTo(BeNil())
+			Expect(err.Error()).To(ContainSubstring("forced error"))
+		})
 	})
 
-	It("should find objects with an $or and a criteria with a erroneous condition", func() {
-		r := &testRepNoDefaultCriteriaNoDefaultSorting{}
-		insertObjects(r)
-		objs := make([]testRepObject, 0)
-		err := repository.FindAll(r, &objs, repository.Or(
-			repository.WithCriteria(
-				&erroneousCondition{},
-			),
-		))
-		Expect(err).NotTo(BeNil())
-		Expect(err.Error()).To(ContainSubstring("forced error"))
-	})
+	Context("Operator: $not", func() {
+		It("should find objects with an $not condition", func() {
+			r := &testRepNoDefaultCriteriaNoDefaultSorting{}
+			insertObjects(r)
+			objs := make([]testRepObject, 0)
+			Expect(repository.FindAll(r, &objs, repository.Not(
+				repository.LT("strength", 7),
+			))).To(BeNil())
+			Expect(objs).To(HaveLen(2))
+			Expect(objs[0].Name).To(Equal("Snake Eyes"))
+			Expect(objs[0].Age).To(Equal(33))
+			Expect(objs[0].Strength).To(Equal(7))
+			Expect(objs[0].Agiliy).To(Equal(9))
+		})
 
-	It("should find objects with an $not condition", func() {
-		r := &testRepNoDefaultCriteriaNoDefaultSorting{}
-		insertObjects(r)
-		objs := make([]testRepObject, 0)
-		Expect(repository.FindAll(r, &objs, repository.Not(
-			repository.LT("strength", 7),
-		))).To(BeNil())
-		Expect(objs).To(HaveLen(2))
-		Expect(objs[0].Name).To(Equal("Snake Eyes"))
-		Expect(objs[0].Age).To(Equal(33))
-		Expect(objs[0].Strength).To(Equal(7))
-		Expect(objs[0].Agiliy).To(Equal(9))
-	})
-
-	It("should find objects with an $not and a criteria with a erroneous condition", func() {
-		r := &testRepNoDefaultCriteriaNoDefaultSorting{}
-		insertObjects(r)
-		objs := make([]testRepObject, 0)
-		err := repository.FindAll(r, &objs, repository.Not(
-			repository.GT("",""),
-		))
-		Expect(err)
+		It("should find objects with an $not and a criteria with a erroneous condition", func() {
+			r := &testRepNoDefaultCriteriaNoDefaultSorting{}
+			insertObjects(r)
+			objs := make([]testRepObject, 0)
+			err := repository.FindAll(r, &objs, repository.Not(
+				repository.GT("", ""),
+			))
+			Expect(err)
+		})
 	})
 
 })
