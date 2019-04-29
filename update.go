9947bd6256476260cd1b98cfadbd718610d13ad0
Felipe Pontes <felipemfpontes@gmail.com>
2019-04-29 11:59:54 -0300

    Add UpdateAndFind method

--- update.go
+++ update.go
@@ -5,6 +5,19 @@ import (
 	"github.com/globalsign/mgo/bson"
 )
 
+// UpdateAndFind does an update and returns the updated model.
+func UpdateAndFind(r Repository, id interface{}, dst interface{}, object interface{}) error {
+	return r.GetQueryRunner().RunWithDB(func(db *mgo.Database) error {
+		_, err := db.C(r.GetCollectionName()).Find(bson.M{"_id": id}).Apply(mgo.Change{
+			Update: bson.M{
+				"$set": object,
+			},
+			ReturnNew: true,
+		}, dst)
+		return err
+	})
+}
+
 // Update does a complete update of a model.
 func Update(r Repository, id interface{}, object interface{}) error {
 	return r.GetQueryRunner().RunWithDB(func(db *mgo.Database) error {
